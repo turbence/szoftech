4a55dc72715627398969ee6739ea679a2ddda5f0
turcsanyi1 <turcsanyi1@IIT114>
2018-09-26 11:41:57 +0200

    end

--- szoftech_gyak2/szoftech_gyak2/Auto.cs
+++ szoftech_gyak2/szoftech_gyak2/Auto.cs
@@ -8,12 +8,118 @@ namespace szoftech_gyak2
 {
     class Auto
     {
-        string gyarto, tipus, motortipus;
-        int ev;
+        private string gyarto;
+        private string tipus;
+        //futási idejű konstans (const az fordítási idejű)
+        private readonly int evjarat;
 
-        static void Main(string[] args)
+        private string motorTipus;
+        private string uzemanyag;
+        private double atlagFogyasztas;
+
+        public double AtlagFogyasztas
         {
+            get { return atlagFogyasztas; }
+            set { atlagFogyasztas = value; }
+        }
 
+
+        public string Uzemanyag
+        {
+            get { return uzemanyag; }
+            set { uzemanyag = value; }
+        }
+
+
+        public string MotorTipus
+        {
+            get { return motorTipus; }
+            set { motorTipus = value; }
+        }
+
+
+        public string Tipus
+        {
+            get { return tipus; }
+            //set { tipus = value; }
+        }
+        public string Gyarto
+        {
+            get { return gyarto; }
+            //set { gyarto = value; }
+        }
+
+        public Auto(string gyarto, string tipus, int evjarat, string motorTipus, string uzemanyag, double atlagFogyasztas)
+        {
+            this.gyarto = gyarto;
+            this.tipus = tipus;
+            this.evjarat = evjarat;
+            this.motorTipus = motorTipus;
+            this.uzemanyag = uzemanyag;
+            this.atlagFogyasztas = atlagFogyasztas;
+
+            
+        }
+        public Auto(string gyarto, string tipus, int evjarat, string motorTipus)
+        {
+            this.gyarto = gyarto;
+            this.tipus = tipus;
+            this.evjarat = evjarat;
+            this.motorTipus = motorTipus;
+            this.uzemanyag = "benzin";
+
+            switch (motorTipus)
+            {
+                case "1.4":
+                    this.atlagFogyasztas = 6.5;
+                    break;
+                case "1.8":
+                    this.atlagFogyasztas = 7.5;
+                    break;
+                case "2.0":
+                    this.atlagFogyasztas = 8;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public void MotorCsere(string motorTipus, string uzemanyag, double atlagFogyasztas)
+        {
+            this.motorTipus = motorTipus;
+            this.uzemanyag = uzemanyag;
+            this.atlagFogyasztas = atlagFogyasztas;
+
+        }
+        public double Fogyasztas(double uzemanyag, int tavolsag)
+        {
+            return uzemanyag / tavolsag * 100;
+        }
+
+        public bool AtlagFogyasztasOsszehasonlito(double uzemanyag, int tavolsag)
+        {
+            return Fogyasztas(uzemanyag, tavolsag) <= atlagFogyasztas;
+        }
+        /// <summary>
+        /// uzemanyag koltseg kiszamitasa
+        /// </summary>
+        /// <param name="tavolsag"> távolság km-ben</param>
+        /// <param name="uzemanyag">uzemanyag literenkénti ára ft-ban</param>
+        /// <returns>uzemanyag osszkoltseg</returns>
+        public int UzemanyagKoltsegSzamitas(int tavolsag, int uzemanyag)
+        {
+            return (int)(atlagFogyasztas * tavolsag * uzemanyag / 100);
+        }
+        public override string ToString()
+        {
+            return "Auto: "
+                + Gyarto + ", "
+                + Tipus + ", "
+                + evjarat + ", "
+                + MotorTipus + ", "
+                + Uzemanyag + ", "
+                + AtlagFogyasztas;
         }
     }
 }
---
+++ szoftech_gyak2/szoftech_gyak2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szoftech_gyak2.futtathato
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int darabszam = 2;
+            Auto[] autok = new Auto[darabszam];
+
+            autok[0] = new Auto("Skoda", "superb", DateTime.Now.Year, "1.8");
+        }
+    }
+}
