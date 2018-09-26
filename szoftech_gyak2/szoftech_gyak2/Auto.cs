de626104bae1bb9bea804653603b20548e44748e
turcsanyi1 <turcsanyi1@IIT114>
2018-09-26 10:46:00 +0200

    teszt

---
+++ szoftech_gyak2/szoftech_gyak2/Auto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szoftech_gyak2
+{
+    class Auto
+    {
+        string gyarto, tipus, motortipus;
+        int ev;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
